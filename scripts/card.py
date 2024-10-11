9485416d68998d395a350bfb12fc5577826c5fbe
cazaugg <f4rr3ll@gmx.ch>
2024-10-11 22:56:40 +0200

    Clean up and structure repo

--- scripts/card.py
+++ scripts/card.py
@@ -1,58 +1,88 @@
 import sqlite3
 import typst
+from pathlib import Path
 
-# Connect to SQLite database
-db_name = 'lp21.db'
-conn = sqlite3.connect(db_name)
-conn.row_factory = sqlite3.Row
-cursor = conn.cursor()
+def get_database(db_path):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    return cursor
 
+def get_goals_by_cycle(db, cycle_code_list):
+    sql="SELECT * FROM lp21 WHERE cycle_code in ({seq})".format(seq=','.join(['?']*len(cycle_code_list)))
+    db.execute(sql, cycle_code_list)
+    rows = db.fetchall()
+    print(f'Searching for goals in {cycle_code_list}')
+    print(f'Found {len(rows)} goals')
+    return rows
 
-cursor.execute('SELECT * FROM lp21 WHERE cycle_code in ("1", "1b", "1,2a", "1,2", "1b,2", "1b,2a")')
+def create_typst_document(rows, typst_template_file='./template/card.typ', target_file='./build/card-set.typ'):
+    file = open(typst_template_file, 'r', encoding='utf-8')
+    card = file.read()
 
-rows = cursor.fetchall()
-print(f'Found {len(rows)} goals')
+    for row in rows:
 
-file_name = './scripts/card.typ'
-file = open(file_name, 'r', encoding='utf-8')
-card = file.read()
+        # We need to split the competence title into two rows as some are very long
+        competence = "\n".join(row['competence_title'].rsplit(".", 1))
+        competence = competence.replace(".", ": ")
 
-for row in rows:
+        # We need to add the preamble to every line 
+        preamble = "Die Schülerinnen und Schüler "
+        text_length = len(row['level_text'])
+        lines =  row['level_text'].split('\n')
+        nofLines = len(lines) - 1
+        
+        # Parse code (so we can append page index if needed)
+        code = row['level_code']
 
-    # We need to split the competence title into two rows as some are very long
-    competence = "\n".join(row['competence_title'].rsplit(".", 1))
-    competence = competence.replace(".", ": ")
+        # If the goal has more than 3 lines, we put the first two on another page
+        text = ""    
+        if nofLines >= 4 or (text_length > 410 and nofLines == 3 ):
+            text = text + preamble + lines[0] + "\n" 
+            text = text + preamble + lines[1] + "\n"    
+            typst_card=f'#card(category: "{competence}", code: "{code} | 1/2", class: "{row['cycle_name']}", ladder: "{row['cycle_ladder']}", color: "{row['subject_color']}",  optional: {row['cycle_optional']}, )[{text}]\r\n'
+            card += typst_card
+            lines = lines[2:]
+            code += " | 2/2"
+        
+        # Assemble a single card normally
+        text = ""
+        for line in lines[0:-1]:
+            text = text + preamble + line + "\n" 
 
-    # We need to add the preamble to every line 
-    preamble = "Die Schülerinnen und Schüler "
-    text_length = len(row['level_text'])
-    lines =  row['level_text'].split('\n')
-    nofLines = len(lines) - 1
-    
-    # Parse code (so we can append page index if needed)
-    code = row['level_code']
-
-    # If the goal has more than 3 lines, we put the first two on another page
-    text = ""    
-    if nofLines >= 4 or (text_length > 410 and nofLines == 3 ):
-        text = text + preamble + lines[0] + "\n" 
-        text = text + preamble + lines[1] + "\n"    
-        typst_card=f'#card(category: "{competence}", code: "{code} | 1/2", class: "{row['cycle_name']}", ladder: "{row['cycle_ladder']}", color: "{row['subject_color']}",  optional: {row['cycle_optional']}, )[{text}]\r\n'
+        # Assmeble the typst function call for a card
+        typst_card=f'#card(category: "{competence}", code: "{code}", class: "{row['cycle_name']}", ladder: "{row['cycle_ladder']}", color: "{row['subject_color']}",  optional: {row['cycle_optional']}, )[{text}]\r\n'
         card += typst_card
-        lines = lines[2:]
-        code += " | 2/2"
+        #print(f'{row['level_code']} Text Length {text_length} on {nofLines} Lines')
+
+    # Put it all into a typst file and compile it
+    Path(target_file).resolve().parent.mkdir(exist_ok=True)
+    with open(target_file, 'w', encoding='utf-8') as card_set:
+        card_set.write(card)
+
+
+pdf_list = [
+    { "name": "Karteikarten Kindergarten", "cycle": ["1"] },
+    { "name": "Karteikarten 1-2. Klasse Grundanforderungen", "cycle": ["1b"] },
+    { "name": "Karteikarten 1-2. Klasse Optional", "cycle": ["1,2a", "1,2", "1b,2a"] },
+    { "name": "Karteikarten 3-4. Klasse Grundanforderungen", "cycle": ["2a", ] },
+    { "name": "Karteikarten 3-4. Klasse Optional", "cycle": ["2", "2,3", "2,3a"] },
+    { "name": "Karteikarten 5-6. Klasse Grundanforderungen", "cycle": ["2b", ] },
+    { "name": "Karteikarten 5-6. Klasse Optional", "cycle": ["2b,3", "2b,3a", "2b,3ao"] },
+    { "name": "Karteikarten 7-8. Klasse Grundanforderungen", "cycle": ["3a", ] },
+    { "name": "Karteikarten 7-8. Klasse Optional", "cycle": ["3", "3o", "3ao"] },
+    { "name": "Karteikarten 9. Klasse Grundanforderungen", "cycle": ["3b", ] },
+    { "name": "Karteikarten 9. Klasse Optional", "cycle": ["3bo"] }
+]
+
+if __name__ == '__main__':
     
-    # Assemble a single card normally
-    text = ""
-    for line in lines[0:-1]:
-        text = text + preamble + line + "\n" 
+    db = get_database('./content/sqlite/lp21.db')
 
-    # Assmeble the typst function call for a card
-    typst_card=f'#card(category: "{competence}", code: "{code}", class: "{row['cycle_name']}", ladder: "{row['cycle_ladder']}", color: "{row['subject_color']}",  optional: {row['cycle_optional']}, )[{text}]\r\n'
-    card += typst_card
-    print(f'{row['level_code']} Text Length {text_length} on {nofLines} Lines')
-
-# Put it all into a typst file and compile it
-with open('card-set.typ', 'w', encoding='utf-8') as card_set:
-    card_set.write(card)
-    typst.compile("card-set.typ", output="card-set.pdf")
+    for document in pdf_list:
+        name = document['name']
+        print(f'Creating document "{name}.pdf"')
+        goals = get_goals_by_cycle(db, document['cycle'])
+        create_typst_document(goals, typst_template_file='./template/card.typ', target_file='./build/' + name + '.typ' )
+        typst.compile("./build/card-set.typ", output="./content/pdf/" + name + '.pdf')
+        print("")
--- scripts/setup-db.py
+++ scripts/setup-db.py
@@ -1,8 +1,10 @@
 import sqlite3
 import json
+import os
 
 # Connect to SQLite database
-db_name = 'lp21.db'
+db_name = './content/sqlite/lp21.db'
+if os.path.exists(db_name): os.remove(db_name)
 conn = sqlite3.connect(db_name)
 cursor = conn.cursor()
 print(f'Created Datase {db_name}')
@@ -11,7 +13,7 @@ print(f'Created Datase {db_name}')
 # Create Database for Competence Levels
 ###############################################################################
 
-file_name = './subjects/LP21.json'
+file_name = './content/json/goals.json'
 file = open(file_name, 'r', encoding='utf-8')
 content = json.load(file)
 
@@ -50,22 +52,9 @@ conn.commit()
 # Create Database for Competences
 ###############################################################################
 
-file_name = './subjects/Kompetenzen.json'
+file_name = './content/json/competences.json'
 file = open(file_name, 'r', encoding='utf-8')
 content = json.load(file)
-
-seen = set()
-dupes = []
-
-for comp in content:
-    code = comp['Code']
-    if code in seen:
-        dupes.append(code)
-    else:
-        seen.add(code)
-
-print(f"Dublicates {dupes}")
-
 
 print(f'Opened file {file_name}')
 print(f'It contains {len(content)} objects')
@@ -95,6 +84,13 @@ conn.commit()
 # Create Database for Subjects
 ###############################################################################
 
+file_name = './content/json/subjects.json'
+file = open(file_name, 'r', encoding='utf-8')
+subjects = json.load(file)
+
+print(f'Opened file {file_name}')
+print(f'It contains {len(subjects)} objects')
+
 create_cycles = '''
 CREATE TABLE IF NOT EXISTS subjects (
     name TEXT NOT NULL,
@@ -105,17 +101,6 @@ CREATE TABLE IF NOT EXISTS subjects (
 )
 '''
 cursor.execute(create_cycles)
-
-subjects = [
-    { "Name": "Deutsch",                            "Nick": "Deutsch",    "Code": "D",   "Color": "#fccdcc"  },
-    { "Name": "Mathematik",                         "Nick": "Math",       "Code": "MA",  "Color": "#cfe4ff"  },
-    { "Name": "Medien und Informatik",              "Nick": "Informatik", "Code": "MI",  "Color": "#cfffe6"  },
-    { "Name": "Musik",                              "Nick": "Musik",      "Code": "MU",  "Color": "#d0ffff"  },
-    { "Name": "Natur, Mensch, Gesellschaft",        "Nick": "NMG",        "Code": "NMG", "Color": "#ceffcc"  },
-    { "Name": "Bewegung und Sport",                 "Nick": "Sport",      "Code": "BS",  "Color": "#fdffcc"  },
-    { "Name": "Textiles und Technisches Gestalten", "Nick": "Werken",     "Code": "TTG", "Color": "#cec9ff"  },
-    { "Name": "Bildnerisches Gestalten",            "Nick": "Zeichnen",   "Code": "BG",  "Color": "#fce7cc"  },
-]
 
 sql = '''
 INSERT INTO subjects (name, nick, code, color)
@@ -132,6 +117,12 @@ conn.commit()
 ###############################################################################
 # Create Database for Cycles
 ###############################################################################
+file_name = './content/json/cycles.json'
+file = open(file_name, 'r', encoding='utf-8')
+cycles = json.load(file)
+
+print(f'Opened file {file_name}')
+print(f'It contains {len(cycles)} objects')
 
 create_cycles = '''
 CREATE TABLE IF NOT EXISTS cycles (
@@ -143,28 +134,6 @@ CREATE TABLE IF NOT EXISTS cycles (
 )
 '''
 cursor.execute(create_cycles)
-
-cycles = [
-    { 'Code': '1',      'Name': 'Kindergarten - 2. Klasse', 'Optional': False, 'Ladder': '12'},
-    { 'Code': '1,2',    'Name': 'Kindergarten - 6. Klasse', 'Optional': False, 'Ladder': '1234'},
-    { 'Code': '1,2a',   'Name': 'Kindergarten - 4. Klasse', 'Optional': False, 'Ladder': '123'},
-    { 'Code': '1b',     'Name': '1. - 2. Klasse',           'Optional': False, 'Ladder': '2'},
-    { 'Code': '1b,2a',  'Name': '1. - 4. Klasse',           'Optional': False, 'Ladder': '23'},
-    { 'Code': '2',      'Name': '3. - 6. Klasse',           'Optional': False, 'Ladder': '34'},
-    { 'Code': '2,3',    'Name': '3. - 9. Klasse',           'Optional': False, 'Ladder': '3456'},
-    { 'Code': '2,3a',   'Name': '3. - 8. Klasse',           'Optional': False, 'Ladder': '345'},
-    { 'Code': '2a',     'Name': '3. - 4. Klasse',           'Optional': False, 'Ladder': '3'},
-    { 'Code': '2b',     'Name': '5. - 6. Klasse',           'Optional': False, 'Ladder': '4'},
-    { 'Code': '2b,3',   'Name': '5. - 9. Klasse',           'Optional': False, 'Ladder': '456'},
-    { 'Code': '2b,3a',  'Name': '5. - 8. Klasse',           'Optional': False, 'Ladder': '45'},
-    { 'Code': '2b,3ao', 'Name': '5. - 8. Klasse',           'Optional': True , 'Ladder': '45'},
-    { 'Code': '3',      'Name': '7. - 9. Klasse',           'Optional': False, 'Ladder': '56'},
-    { 'Code': '3a',     'Name': '7. - 8. Klasse',           'Optional': False, 'Ladder': '5'},
-    { 'Code': '3ao',    'Name': '7. - 8. Klasse',           'Optional': True , 'Ladder': '5'},
-    { 'Code': '3b',     'Name': '9. Klasse',                'Optional': False, 'Ladder': '6'},
-    { 'Code': '3bo',    'Name': '9. Klasse',                'Optional': True , 'Ladder': '6'},
-    { 'Code': '3o',     'Name': '7. - 9. Klasse',           'Optional': True , 'Ladder': '56'}
-]
 
 sql = '''
 INSERT INTO cycles (code, name, ladder, optional)
@@ -192,7 +161,6 @@ INNER JOIN subjects ON levels.subject=subjects.code
 INNER JOIN competences ON levels.competence=competences.code
 INNER JOIN cycles ON levels.cycle=cycles.code
 '''
-
 cursor.execute(sql)
 conn.commit()
 
