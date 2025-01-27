fcdf886f6669d66daa7e20be6f12e66f105ebc2a
cazaugg <f4rr3ll@gmx.ch>
2025-01-27 20:40:33 +0100

    Add stats

--- scripts/report.py
+++ scripts/report.py
@@ -24,4 +24,30 @@ for row in cursor.fetchall():
 print(text)
 
 
+cursor.execute("SELECT code from cycles")
+cycles = cursor.fetchall()
 
+cursor.execute("SELECT code from subjects")
+subjects = cursor.fetchall()
+
+text = "| Fach \ Zyklus |"
+for cycle in cycles:
+    text += " " + cycle[0] + " |"
+print(text)
+print( "| --- |" + " --- |" * len(cycles))
+
+for subject in subjects:
+    text = "| " + subject[0] + " |"
+    for cycle in cycles:
+        cursor.execute('SELECT level_code, count(level_code), subject_code, cycle_code from LP21 where subject_code=? and cycle_code=?', [subject[0], cycle[0]])
+        for row in cursor.fetchall():
+            text += f' {row[1]} |'
+    print(text)
+
+
+text = "| Total |"
+for cycle in cycles:
+    cursor.execute('SELECT level_code, count(level_code), subject_code, cycle_code from LP21 where cycle_code=?', [cycle[0]])
+    for row in cursor.fetchall():
+        text += f' {row[1]} |'
+print(text)
